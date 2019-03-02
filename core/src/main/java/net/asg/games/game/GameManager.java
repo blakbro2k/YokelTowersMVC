619a3058b88e3e9337e1eecbb5dd316a86e665f2
blakbro2k <boaten01@gmail.com>
2019-03-01 19:45:47 -0500

    testing internal server functions

--- core/src/main/java/net/asg/games/game/GameManager.java
+++ core/src/main/java/net/asg/games/game/GameManager.java
@@ -1,26 +1,20 @@
 package net.asg.games.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
-import com.badlogic.gdx.utils.Json;
 import com.github.czyzby.autumn.annotation.Component;
 import com.github.czyzby.autumn.annotation.Initiate;
 import com.github.czyzby.autumn.annotation.Inject;
-import com.github.czyzby.autumn.annotation.OnMessage;
 import com.github.czyzby.autumn.mvc.component.ui.InterfaceService;
-import com.github.czyzby.autumn.mvc.config.AutumnMessage;
-import com.github.czyzby.autumn.mvc.stereotype.Asset;
-import com.github.czyzby.lml.annotation.LmlActor;
 
 import net.asg.games.controller.UITestController;
+import net.asg.games.service.UserInterfaceService;
 
 @Component
 public class GameManager {
-    @Inject private InterfaceService interfaceService;
+    @Inject private UserInterfaceService uiService;
     @Inject private UITestController uiView;
 
     @Initiate
@@ -30,19 +24,13 @@ public class GameManager {
     }
 
     public void initiateGame() {
-        System.out.println("initiateGame");
-        System.out.println("uiView=" + uiView);
-        System.out.println("uiView.bashBlockImage=" + uiView.bashBlockImage);
-
-        uiView.yBlockImage.setDrawable(interfaceService.getSkin(), "Y_block");
-        uiView.oBlockImage.setDrawable(interfaceService.getSkin(), "O_block");
-        uiView.kBlockImage.setDrawable(interfaceService.getSkin(), "K_block");
-        uiView.eBlockImage.setDrawable(interfaceService.getSkin(), "E_block");
-        uiView.lBlockImage.setDrawable(interfaceService.getSkin(), "L_block");
-        uiView.bashBlockImage.setDrawable(interfaceService.getSkin(), "Bash_block");
-        System.out.println("Regions=" + getRegions("defense_Bash_block"));
-        System.out.println("Regions=" + interfaceService.getSkin().getRegions("defense_Bash_block"));
-        System.out.println("retion(Bash_block_Broken_2)=" + interfaceService.getSkin().getRegion("Bash_block_Broken_2"));
+        uiService.loadDrawable(uiView.yBlockImage);
+        uiService.loadDrawable(uiView.oBlockImage);
+        uiService.loadDrawable(uiView.kBlockImage);
+        uiService.loadDrawable(uiView.eBlockImage);
+        uiService.loadDrawable(uiView.lBlockImage);
+        uiService.loadDrawable(uiView.bashBlockImage);
+        uiService.loadDrawable(uiView.defenseYBlockImage);
     }
 
     public void update(){}
@@ -51,34 +39,5 @@ public class GameManager {
     public void handleSetPiece(){}
     public String[] getBoardState(){ return null;}
 
-    public Array<TextureRegion> getRegions(String regionName) {
-        int i = 0;
-        regionName +=  "_";
-        TextureRegion region = getRegion(regionName, i);
 
-        Array<TextureRegion> regions = null;
-        while(region != null){
-            i++;
-            region = getRegion(regionName, i);
-            addRegion(regions,region);
-        }
-        return regions;
-    }
-
-    private TextureRegion getRegion(String regionNames, int i){
-        try{
-            return interfaceService.getSkin().getRegion(regionNames + i);
-        } catch(GdxRuntimeException e) {
-            return null;
-        }
-    }
-
-    private void addRegion(Array<TextureRegion> regions, TextureRegion region){
-        if(region != null){
-            if(regions == null){
-                regions = new Array<>();
-            }
-            regions.add(region);
-        }
-    }
 }
--- core/src/main/java/net/asg/games/service/UserInterfaceService.java
+++ core/src/main/java/net/asg/games/service/UserInterfaceService.java
@@ -2,7 +2,11 @@ package net.asg.games.service;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.github.czyzby.autumn.annotation.Component;
 import com.github.czyzby.autumn.annotation.Initiate;
@@ -10,9 +14,9 @@ import com.github.czyzby.autumn.annotation.Inject;
 import com.github.czyzby.autumn.mvc.component.asset.AssetService;
 import com.github.czyzby.autumn.mvc.component.ui.InterfaceService;
 import com.github.czyzby.autumn.mvc.component.ui.SkinService;
+import com.github.czyzby.lml.scene2d.ui.reflected.AnimatedImage;
 
 import net.asg.games.game.objects.YokelBlock;
-import net.asg.games.provider.actors.AnimatedImage;
 import net.asg.games.provider.actors.GameBlock;
 
 
@@ -33,60 +37,51 @@ public class UserInterfaceService {
         return null;
     }
 
-    public AnimatedImage createAnimatedImage(String name, boolean looping){
-        return new AnimatedImage(createAnimation(name), looping);
-    }
-
-    public Animation<TextureRegion> createAnimation(String regionName) {
-        Array<TextureRegion> regions = getTextureRegions(regionName);
-        if(regions != null){
-            return createAnimation(regionName, regions.size);
-        }
-        return null;
-    }
-
-        public Animation<TextureRegion> createAnimation(String textureName, int maxIndex) {
-        if(maxIndex < -1 || maxIndex == 0){
-            throw new ArrayIndexOutOfBoundsException("maxIndex: " + maxIndex);
-        }
-        Animation<TextureRegion> animation = animationMap.get(textureName);
-
-        if(animation == null){
-            TextureRegion[] runningFrames;
-            if(maxIndex == -1){
-                runningFrames = new TextureRegion[1];
-                runningFrames[0] = getTextureRegion(textureName,-1);
-            } else {
-                runningFrames = new TextureRegion[maxIndex];
-                for (int i = 1; i < maxIndex + 1; i++) {
-                    runningFrames[i-1] = getTextureRegion(textureName,i);
-                }
-            }
-
-            animation = new Animation<TextureRegion>(1.0f, runningFrames);
-            animationMap.put(textureName, animation);
-            return animation;
-        } else {
-            return animation;
+    public void loadDrawable(Actor actor){
+        if(actor != null){
+            String actorName = actor.getName();
+            loadDrawable(actor, actorName);
         }
     }
 
-    public TextureRegion getTextureRegion(String regionName, int index) {
-        return getRegionIndex(getTextureRegions(regionName), index);
-    }
-
-    private TextureRegion getRegionIndex(Array<TextureRegion> regions, int index){
-        if(regions != null){
+    private void loadDrawable(Actor actor, String name){
+        if(actor instanceof Image){
+            Image image = (Image) actor;
             try{
-                return regions.get(index);
-            } catch (IndexOutOfBoundsException iobe){
-                return null;
+                image.setDrawable(interfaceService.getSkin(), name);
+            } catch(GdxRuntimeException ge){
+                ge.printStackTrace();
             }
         }
-        return null;
+        if(actor instanceof AnimatedImage){
+            AnimatedImage image = (AnimatedImage) actor;
+            try{
+                image.setFrames(getDrawableFrames(name));
+            } catch(GdxRuntimeException ge){
+                ge.printStackTrace();
+            }
+        }
     }
 
-    public Array<TextureRegion> getTextureRegions(String regionName){
-        return interfaceService.getSkin().getRegions(regionName);
+    public Array<Drawable> getDrawableFrames(String regionName) {
+        int i = 1;
+        regionName +=  "_";
+        Drawable drawable = getDrawable(regionName, i);
+
+        Array<Drawable> drawables = new Array<>();
+        while(drawable != null){
+            drawables.add(drawable);
+            i++;
+            drawable = getDrawable(regionName, i);
+        }
+        return drawables;
+    }
+
+    public Drawable getDrawable(String regionNames, int i){
+        try{
+            return interfaceService.getSkin().getDrawable(regionNames + i);
+        } catch(GdxRuntimeException e) {
+            return null;
+        }
     }
 }
