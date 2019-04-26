76b422e7a0dc82a7ffa21d678a3e154fc401d817
eboateng <boaten01@gmail.com>
2019-04-26 17:46:31 -0400

    Added board number to the background.

--- core/src/main/java/net/asg/games/provider/actors/GameBlockArea.java
+++ core/src/main/java/net/asg/games/provider/actors/GameBlockArea.java
@@ -1,18 +1,25 @@
 package net.asg.games.provider.actors;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 
 import net.asg.games.game.objects.YokelBlock;
 import net.asg.games.game.objects.YokelGameBoard;
 import net.asg.games.game.objects.YokelObjectFactory;
+import net.asg.games.utils.Util;
 
 public class GameBlockArea extends Table {
     private static final String CELL_ATTR = "uiCell";
     private static final String CELL_ATTR_SEPARATOR = "_";
+    private static final Color ACTIVE_BACKGROUND_COLOR = new Color(0.7f, 0.7f, 0.7f, 1);
+    private static final Color DEFAULT_BACKGROUND_COLOR = new Color(0.3f, 0.3f, 0.3f, 1);
 
     private static final float BLOCK_DROP_SPEED = .8f;
     private static final float MAX_BLOCK_DROP_SPEED = 6f;
@@ -21,12 +28,13 @@ public class GameBlockArea extends Table {
     private boolean isSpeedDown;
     private boolean isActive;
     private YokelGameBoard board;
-    private Table blockAll;
+    private Table boarder;
 
     private int boardNumber;
     private GamePiece gamePiece;
     private YokelObjectFactory factory;
     private ObjectMap<String, GameBlock> uiBlocks;
+    private Array<GameBlock> sprites;
 
     public GameBlockArea(YokelObjectFactory factory) {
         if (factory == null){
@@ -36,18 +44,28 @@ public class GameBlockArea extends Table {
         if (skin == null){
             throw new GdxRuntimeException("skin cannot be null.");
         }
-        initializeBoard(factory);
         setSkin(skin);
-        setSize(getPrefWidth() + 10, getPrefHeight() + 10);
 
-        setBackground(factory.getGameBlock(YokelBlock.ATTACK_Y).getImage().getDrawable());
+        boarder = new Table();
+        boarder.setDebug(this.getDebug());
+        boarder.setSize(getWidth() + 20, getHeight() + 20);
+        boarder.setBounds(this.getX(), this.getY(), getWidth() - 8, getHeight() - 8);
+        this.setColor(DEFAULT_BACKGROUND_COLOR);
+
+        //add(boarder);
+
+        NinePatch patch = new NinePatch(getSkin().getRegion("8_digit"),3, 3, 3, 3);
+        NinePatchDrawable background = new NinePatchDrawable(patch);
+
+        initializeBoard(factory);
     }
 
     private void initializeBoard(YokelObjectFactory factory){
         this.factory = factory;
         uiBlocks = new ObjectMap<>();
-        initializeUiCells();
+        this.sprites = new Array<>();
         this.boardNumber = 0;
+        initializeUiCells();
     }
 
     private void initializeUiCells(){
@@ -74,6 +92,7 @@ public class GameBlockArea extends Table {
     }
 
     public void setBoardNumber(int number){
+        this.setBackground(number + "_digit");
         this.boardNumber = number;
     }
 
@@ -105,7 +124,9 @@ public class GameBlockArea extends Table {
     @Override
     public void draw(Batch batch, float alpha){
         //if(!isActive) return;
+        boarder.draw(batch, alpha);
         super.draw(batch, alpha);
+        drawSprites(batch, alpha);
     }
 
     public void updateData(YokelGameBoard gameBoard) {
@@ -120,6 +141,16 @@ public class GameBlockArea extends Table {
         for(int r = 0; r < YokelGameBoard.MAX_ROWS; r++){
             for(int c = 0; c < YokelGameBoard.MAX_COLS; c++){
                 setBlock(cells[r][c], r, c);
+            }
+        }
+    }
+
+    private void drawSprites(Batch batch, float alpha){
+        if(!Util.isArrayEmpty(sprites)){
+            for(GameBlock sprite : sprites){
+               if(sprite != null){
+                   sprite.draw(batch, alpha);
+               }
             }
         }
     }
