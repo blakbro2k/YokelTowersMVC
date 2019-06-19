c86f939ecd84645b6b814e16ad28ef6e72f230a3
blakbro2k <boaten01@gmail.com>
2019-06-19 17:18:44 -0400

    testing server functionality

---
+++ desktop/test/net/asg/games/YokelTowersUITest.java
@@ -0,0 +1,25 @@
+package net.asg.games;
+
+import com.github.czyzby.websocket.WebSocket;
+
+import junit.framework.TestFailure;
+
+import net.asg.games.desktop.DesktopLauncher;
+import net.asg.games.game.ServerManager;
+import net.asg.games.utils.TestingUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.lang.reflect.InvocationTargetException;
+
+@RunWith(DesktopLauncher.class)
+public class YokelTowersUITest {
+
+    @Test
+    public void loungeUITest() throws Throwable {
+        throw new Throwable("Test Case Not Initialized");
+    }
+}
